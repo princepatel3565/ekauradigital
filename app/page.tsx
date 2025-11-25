72355ee97ea939adb85e84565ea333e7d3e4875d
princepatel3565 <princepatel3525@gmail.com>
2025-11-25 13:56:08 +0530

    innitial changes

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+<div className="warning-content">
+    <h1>We're under construction</h1>
+    <p>
+      We're carefully crafting our new website. Coming soon with a faster, more elegant, and much more powerful experience.
+    </p>
+    <p>
+      This site is being prepared by <span>Ekaura Digital.</span></p>
+  </div>
+  );
+}
